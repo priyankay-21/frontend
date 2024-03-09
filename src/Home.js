253e1ba67aa5e02a8ea6cbc824561defecaa6454
priyankay-21 <priyankapriyat@gmail.com>
2024-03-09 03:51:22 -0800

    first commit

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://static.vecteezy.com/system/resources/thumbnails/024/472/833/small_2x/green-earth-day-on-isolated-white-background-care-for-the-environment-and-ecology-resources-that-are-sustainable-renewable-and-green-graphic-resource-generative-ai-free-photo.jpg" alt="Welcome picture" />
+    </div>
+  )
